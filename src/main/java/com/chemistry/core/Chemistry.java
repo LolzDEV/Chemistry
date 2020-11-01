e3fe025b1eba125a89d0bd59182b51d79de85dae
LolzDEV <lollotorres06@gmail.com>
2020-11-01 18:51:00 +0100

    Workspace setup

---
+++ src/main/java/com/chemistry/core/Chemistry.java
@@ -0,0 +1,10 @@
+package com.chemistry.core;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Chemistry implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
