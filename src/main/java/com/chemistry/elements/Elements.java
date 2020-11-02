fec1c1790432164a83776c08eb2bde49ee2bd492
LolzDEV <lollotorres06@gmail.com>
2020-11-02 15:48:48 +0100

    Added elements from 15 to 57

--- src/main/java/com/chemistry/elements/Elements.java
+++ src/main/java/com/chemistry/elements/Elements.java
@@ -24,6 +24,50 @@ public class Elements {
         elements.put("magnesium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Magnesium", "Mg", 12, ChemicalType.SOLID));
         elements.put("aluminium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Aluminium", "Al", 13, ChemicalType.SOLID));
         elements.put("silicon", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Silicon", "Si", 14, ChemicalType.SOLID));
+        elements.put("phosphor", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Phosphor", "P", 15, ChemicalType.SOLID));
+        elements.put("sulfur", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Sulfur", "S", 16, ChemicalType.SOLID));
+        elements.put("chlorine", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Chlorine", "Cl", 17, ChemicalType.LIQUID));
+        elements.put("argon", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Argon", "Ar", 18, ChemicalType.GAS));
+        elements.put("potassium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Potassium", "K", 19, ChemicalType.SOLID));
+        elements.put("calcium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Calcium", "Ca", 20, ChemicalType.SOLID));
+        elements.put("scandium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Scandium", "Sc", 21, ChemicalType.SOLID));
+        elements.put("titanium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Titanium", "Ti", 22, ChemicalType.SOLID));
+        elements.put("vanadium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Vanadium", "V", 23, ChemicalType.SOLID));
+        elements.put("chromium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Chromium", "Cr", 24, ChemicalType.SOLID));
+        elements.put("manganese", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Manganese", "Mn", 25, ChemicalType.SOLID));
+        elements.put("iron", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Iron", "Fe", 26, ChemicalType.SOLID));
+        elements.put("cobalt", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Cobalt", "Co", 27, ChemicalType.SOLID));
+        elements.put("nickel", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Nickel", "Ni", 28, ChemicalType.SOLID));
+        elements.put("copper", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Copper", "Cu", 29, ChemicalType.SOLID));
+        elements.put("zinc", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Zinc", "Zn", 30, ChemicalType.SOLID));
+        elements.put("gallium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Gallium", "Ga", 31, ChemicalType.SOLID));
+        elements.put("germanium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Germanium", "Ge", 32, ChemicalType.SOLID));
+        elements.put("arsenic", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Arsenic", "As", 33, ChemicalType.SOLID));
+        elements.put("selenium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Selenium", "Se", 34, ChemicalType.SOLID));
+        elements.put("bromine", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Bromine", "Br", 35, ChemicalType.LIQUID));
+        elements.put("krypton", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Krypton", "Kr", 36, ChemicalType.GAS));
+        elements.put("rubidium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Rubidium", "Rb", 37, ChemicalType.SOLID));
+        elements.put("strontium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Strontium", "Sr", 38, ChemicalType.SOLID));
+        elements.put("yttrium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Yttrium", "Y", 39, ChemicalType.SOLID));
+        elements.put("zincronium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Zincronium", "Zr", 40, ChemicalType.SOLID));
+        elements.put("niobium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Niobium", "Nb", 41, ChemicalType.SOLID));
+        elements.put("molybdenum", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Molybdenum", "Mo", 42, ChemicalType.SOLID));
+        elements.put("technetium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Technetium", "Tc", 43, ChemicalType.SOLID));
+        elements.put("ruthenium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Ruthenium", "Ru", 44, ChemicalType.SOLID));
+        elements.put("rhodium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Rhodium", "Rh", 45, ChemicalType.SOLID));
+        elements.put("palladium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Palladium", "Pd", 46, ChemicalType.SOLID));
+        elements.put("silver", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Silver", "Ag", 47, ChemicalType.SOLID));
+        elements.put("cadmium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Cadmium", "Cd", 48, ChemicalType.SOLID));
+        elements.put("indium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Indium", "In", 49, ChemicalType.SOLID));
+        elements.put("tin", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Tin", "Sn", 50, ChemicalType.SOLID));
+        elements.put("antimony", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Antimony", "Sb", 51, ChemicalType.SOLID));
+        elements.put("tellurium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Tellurium", "Te", 52, ChemicalType.SOLID));
+        elements.put("iodine", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Iodine", "I", 53, ChemicalType.SOLID));
+        elements.put("xenon", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Xenon", "Xe", 54, ChemicalType.GAS));
+        elements.put("cesium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Cesium", "Cs", 55, ChemicalType.SOLID));
+        elements.put("barium", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Barium", "Ba", 56, ChemicalType.SOLID));
+        elements.put("lanthanum", new ItemElement(new FabricItemSettings().group(ItemGroups.CHEMISTRY_ELEMENTS), "Lanthanum", "La", 57, ChemicalType.SOLID));
+
     }
 
 }
--- src/main/java/com/chemistry/items/ItemGroups.java
+++ src/main/java/com/chemistry/items/ItemGroups.java
@@ -1,16 +1,24 @@
 package com.chemistry.items;
 
+import com.chemistry.api.ItemElement;
 import com.chemistry.elements.Elements;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class ItemGroups {
     public static final ItemGroup CHEMISTRY_ITEMS = FabricItemGroupBuilder.build(
             new Identifier("chemistry", "items"),
             () -> new ItemStack(Items.TUBE));
-    public static final ItemGroup CHEMISTRY_ELEMENTS = FabricItemGroupBuilder.build(
-            new Identifier("chemistry", "elements"),
-            () -> new ItemStack(Elements.elements.get("hydrogen")));
+    public static final ItemGroup CHEMISTRY_ELEMENTS = FabricItemGroupBuilder.create(
+            new Identifier("chemistry", "elements")).icon(() -> new ItemStack(Elements.elements.get("hydrogen"))).appendItems(itemStacks -> {
+            TreeMap<Integer, ItemElement> sorted = new TreeMap<>();
+            for(Map.Entry<String, ItemElement> entry : Elements.elements.entrySet()) sorted.put(entry.getValue().getAtomicNumber(), entry.getValue());
+            for(Map.Entry<Integer, ItemElement> entry : sorted.entrySet()) itemStacks.add(new ItemStack(entry.getValue()));
+
+    }).build();
 }
